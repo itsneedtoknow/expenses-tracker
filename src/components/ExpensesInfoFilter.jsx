c90ec9f046b967a822caf81fc09edd17b1ad81a3
itsneedtoknow <letta_in_love@mail.ru>
2026-05-11 15:10:07 +0300

    feat: add styles for filter

--- src/components/ExpensesInfoFilter.jsx
+++ src/components/ExpensesInfoFilter.jsx
@@ -1,6 +1,7 @@
+import styles from "./ExpensesInfoFilter.module.css";
 export function ExpensesInfoFilter() {
   return (
-    <div className="expenses__filter">
+    <div className={styles.expenses__filter}>
       <h3>Показать</h3>
       <select>Все</select>
       <select>Категория</select>
